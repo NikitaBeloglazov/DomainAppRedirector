37fbf1631e763581a26c48c0c5339827625ff8a5
Nikita Beloglazov <nzadne13@gmail.com>
2023-06-10 03:01:19 +0300

    Pylint fixes, add license comment
    Your code has been rated at 10.00/10 (previous run: 9.29/10, +0.71)
    
    :))

--- domainredirect.py
+++ domainredirect.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3.10
 # -*- coding: utf-8 -*-
+"""
+ * Copyright (C) 2023 Nikita Beloglazov <nnikita.beloglazov@gmail.com>
+ *
+ * This file is part of NikitaBeloglazov/DomainAppRedirector.
+ *
+ * NikitaBeloglazov/DomainAppRedirector is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License v3.0
+ * published by the Mozilla Foundation.
+ *
+ * NikitaBeloglazov/DomainAppRedirector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with NikitaBeloglazov/DomainAppRedirector
+ * If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+"""
 import argparse
 import subprocess
 from urllib.parse import urlparse
@@ -37,7 +53,6 @@ def roadmap_url(domain):
 	Specifically sets specific rules for domains and distributes automated ones via roadmap.
 	Placed in a function for convenience
 	"""
-
 	# - = - = FILTER (www.)youtube.com/(watch|playlist) ONLY. For example youtube.com/user redirects in browser.
 	if domain in ("www.youtube.com", "youtube.com") and "_youtube.com" in roadmap:
 		path = urlparse_result[2] # extract path=
@@ -45,14 +60,16 @@ def roadmap_url(domain):
 			return roadmap["_youtube.com"]
 		del path
 		# else: pass
-	# - = - = - = - = - = - = - = - = - = - = - = 
+	# - = - = - = - = - = - = - = - = - = - = - =
 
+	# redirect domains without specific rules
 	if domain in roadmap:
 		return roadmap[domain]
 	return roadmap["rest"]
 
 open_with = roadmap_url(domain)
 
-# and just start binary!:)
-print(f"Launch Options: {str(open_with+[url])}")
-subprocess.Popen(open_with+[url]) # +unknown (?)
+# and just start binary with flags!:)
+print(f"Launch Options: {str(open_with+[url])}") # +unknown (?)
+with subprocess.Popen(open_with+[url]) as _:
+	pass
